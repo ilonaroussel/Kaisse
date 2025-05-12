34c9af187dc5e597dfd4cbdc91e0963494fe2885
Matthieu Laurent <ml.laurent.matthieu@gmail.com>
2025-05-12 22:19:13 +0200

    Initialize project

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module org.example.kaisse {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens org.example.kaisse to javafx.fxml;
+    exports org.example.kaisse;
+}
