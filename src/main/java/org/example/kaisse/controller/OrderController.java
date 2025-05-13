899fedad4de2dd6f501d291b5cbf318dd664985c
Kaylana1968 <144374760+Kaylana1968@users.noreply.github.com>
2025-05-13 23:17:03 +0200

    add: replaced textField to choose table by a choiceBox + error handling + update listView when adding an order

--- src/main/java/org/example/kaisse/controller/OrderController.java
+++ src/main/java/org/example/kaisse/controller/OrderController.java
@@ -8,6 +8,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
@@ -29,50 +30,84 @@ public class OrderController implements Initializable {
     @FXML
     ListView<HBox> orderList;
     @FXML
-    private TextField tableNumber;
+    private ChoiceBox<String> tableNumber;
+
+    private final ObservableList<HBox> orderListItems = FXCollections.observableArrayList();
 
     @FXML
     public void initialize(URL location, ResourceBundle resources) {
-        MongoCollection<Document> collection = Main.database.getCollection("Order");
+        // Init the ListView of orders
+        MongoCollection<Document> orderCollection = Main.database.getCollection("Order");
 
         List<Bson> pipeline = Arrays.asList(
                 match(eq("state", "PENDING")),
                 lookup("Table", "table", "_id", "table"),
                 lookup("Dish", "dishes", "_id", "dishes"));
 
-        List<Document> documents = collection.aggregate(pipeline).into(new ArrayList<>());
+        List<Document> orderDocuments = orderCollection.aggregate(pipeline).into(new ArrayList<>());
 
-        ObservableList<HBox> items = FXCollections.observableArrayList();
-
-        documents.stream().forEach(element -> {
+        orderDocuments.stream().forEach(element -> {
             Order order = Order.createFromDocument(element);
 
-            HBox box = new HBox(20);
-            box.setPrefHeight(100);
-            box.setPrefWidth(200);
-
-            Label tableLabel = new Label(order.getTable().getNumber().toString());
-            Label dateLabel = new Label(order.getDate().format(DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss")));
-
-            box.getChildren().addAll(tableLabel, dateLabel);
-
-            items.add(box);
+            addOrderInList(order);
         });
 
-        orderList.setItems(items);
+        orderList.setItems(orderListItems);
+
+        // Init the choices of the ChoiceBox
+        MongoCollection<Document> tableCollection = Main.database.getCollection("Table");
+
+        ObservableList<String> tableNumbers = FXCollections.observableArrayList();
+        List<Document> tableDocuments = tableCollection.find().into(new ArrayList<>());
+
+        tableDocuments.stream().forEach(element -> tableNumbers.add(element.getString("number")));
+
+        tableNumber.setItems(tableNumbers);
+    }
+
+    private void addOrderInList(Order order) {
+        HBox box = new HBox(20);
+        box.setPrefHeight(100);
+        box.setPrefWidth(200);
+
+        Label tableLabel = new Label(order.getTable().getNumber().toString());
+        Label dateLabel = new Label(order.getDate().format(DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss")));
+
+        box.getChildren().addAll(tableLabel, dateLabel);
+
+        orderListItems.add(box);
     }
 
     @FXML
     public void createOrder() {
-        MongoCollection<Document> collection = Main.database.getCollection("Table");
+        String tableNumberValue = tableNumber.getValue();
+        // if no table chosen
+        if (tableNumberValue == null) {
+            throw new IllegalArgumentException("No table selected");
+        }
 
-        Document document = collection.find(eq("number", tableNumber.getText())).first();
-        assert document != null;
+        System.out.println(tableNumberValue);
+
+        MongoCollection<Document> tableCollection = Main.database.getCollection("Table");
+
+        Document document = tableCollection.find(eq("number", tableNumberValue)).first();
+        if (document == null) {
+            throw new IllegalStateException("Table not found in database");
+        }
 
         Table table = Table.createFromDocument(document);
 
-        System.out.println(document);
+        MongoCollection<Document> orderCollection = Main.database.getCollection("Order");
 
-        new Order("PENDING", table, LocalDateTime.now(), new ArrayList<>()).insertToDb();
+        // if an order on the table is already ongoing
+        if (orderCollection.find(eq("table", table.getId())).first() != null) {
+            throw new IllegalStateException("An order for this table is already ongoing");
+        }
+
+        // create a PENDING order
+        Order order = new Order("PENDING", table, LocalDateTime.now(), new ArrayList<>());
+
+        order.insertToDb(); // update db
+        addOrderInList(order); // update orderList
     }
 }
