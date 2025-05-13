e742b4596f99b522923e8176ba366c9ccf67ecf4
Ilona Roussel <fleur838@gmail.com>
2025-05-13 16:08:39 +0200

    envoi du formulaire des plats

--- src/main/java/org/example/kaisse/controller/MenuController.java
+++ src/main/java/org/example/kaisse/controller/MenuController.java
@@ -1,16 +1,60 @@
 package org.example.kaisse.controller;
 
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
 import javafx.fxml.FXML;
-import javafx.scene.control.DialogPane;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import org.bson.Document;
 
 public class MenuController {
+
     @FXML
     public GridPane form_popup;
 
+    @FXML private TextField nameField;
+    @FXML private TextArea descriptionField;
+    @FXML private TextField priceField;
+    @FXML private TextField imageField;
+
     @FXML
-    protected void onDishButtonClick(){
+    protected void onDishButtonClick() {
         form_popup.setVisible(!form_popup.isVisible());
-    };
+    }
+    @FXML
+    public void addDish() {
+        try {
+            String name = nameField.getText();
+            String description = descriptionField.getText();
+            float price = Float.parseFloat(priceField.getText());
+            String image = imageField.getText();
+
+            MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017");
+            MongoDatabase database = mongoClient.getDatabase("Kaisse");
+            MongoCollection<Document> collection = database.getCollection("dish");
+
+            Document dish = new Document("name", name)
+                    .append("description", description)
+                    .append("price", price)
+                    .append("image", image);
+
+            collection.insertOne(dish);
+            System.out.println("✅ Plat ajouté avec succès !");
+            mongoClient.close();
+
+            // Reset des champs (optionnel)
+            nameField.clear();
+            descriptionField.clear();
+            priceField.clear();
+            imageField.clear();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 
 }
