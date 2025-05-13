3262ea7b1214cb7edda8ac4b2a98bbf0b3c46033
Ilona Roussel <fleur838@gmail.com>
2025-05-13 16:19:15 +0200

    Envoi du formulaire des plats fonctionnel

--- src/main/java/org/example/kaisse/controller/MenuController.java
+++ src/main/java/org/example/kaisse/controller/MenuController.java
@@ -1,7 +1,5 @@
 package org.example.kaisse.controller;
 
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import javafx.fxml.FXML;
@@ -9,6 +7,7 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import org.bson.Document;
+import org.example.kaisse.Main;
 
 public class MenuController {
 
@@ -26,33 +25,30 @@ public class MenuController {
     }
     @FXML
     public void addDish() {
-        try {
             String name = nameField.getText();
             String description = descriptionField.getText();
             float price = Float.parseFloat(priceField.getText());
             String image = imageField.getText();
 
-            MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017");
-            MongoDatabase database = mongoClient.getDatabase("Kaisse");
-            MongoCollection<Document> collection = database.getCollection("dish");
+            MongoDatabase database = Main.database;
+            MongoCollection<Document> collection = database.getCollection("Dish");
 
             Document dish = new Document("name", name)
                     .append("description", description)
                     .append("price", price)
                     .append("image", image);
 
-            collection.insertOne(dish);
-            System.out.println("✅ Plat ajouté avec succès !");
-            mongoClient.close();
-
-            // Reset des champs (optionnel)
+            //Réinitialise les champs
             nameField.clear();
             descriptionField.clear();
             priceField.clear();
             imageField.clear();
 
+        try {
+            collection.insertOne(dish);
+            System.out.println("Plat ajouté");
         } catch (Exception e) {
-            e.printStackTrace();
+            System.out.println("Fail Insert: " + e);
         }
     }
 
