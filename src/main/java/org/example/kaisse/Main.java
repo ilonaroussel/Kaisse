2a6f61e40a92f1b88d3737faf29f1ace9cf8350e
Kaylana1968 <144374760+Kaylana1968@users.noreply.github.com>
2025-05-14 09:24:51 +0200

    add: load automatically in full screen

--- src/main/java/org/example/kaisse/Main.java
+++ src/main/java/org/example/kaisse/Main.java
@@ -20,9 +20,10 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("main-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Kaisse!");
         stage.setScene(scene);
+        stage.setMaximized(true);
         stage.show();
     }
 
