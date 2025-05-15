8a7ceb0dec9dc698b8649e1838ef270103ff08c3
Nicolas <nico.dobosz@gmail.com>
2025-05-15 16:23:38 +0200

    fix: css of order

--- src/main/java/org/example/kaisse/Main.java
+++ src/main/java/org/example/kaisse/Main.java
@@ -20,7 +20,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("main-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("login-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
 
         scene.getStylesheets().add(
